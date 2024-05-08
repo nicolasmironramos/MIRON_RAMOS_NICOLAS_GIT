2d33a9925ab4f4308a64a8a5290fc73d334fb961
nicolasmironramos <144922077+nicolasmironramos@users.noreply.github.com>
2024-05-08 16:33:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-# 1) Carga de librerías
-    # Cálculo de tiempo de ejecución:
+# 1) Carga de librerías:
+    # Cálculo de tiempo de ejecución
 import time
-    # General:
+    # General
 from src.constantes import *
 from src.funciones import *
 from src.clases import *
 
 # 2) Carga de datos:
-    # Registra el tiempo de inicio
+    # Se registra el tiempo de inicio
 tiempo_inicio = time.time()
 archivo_csv = '/ruta_de_datos/.csv'  # Introducir ruta
-    # Crear una instancia de CSVReader
+    # Se crea una instancia de CSVReader
 csv_reader = LectorCSV(archivo_csv)
 df = csv_reader.crear_dataframe()
-    # Cambiao de los nombres de las columnas
+    # Se cambian los nombres de las columnas
 df = df.rename(columns={"Unnamed: 0": "ID", "text": "TEXTO", "label": "CATEGORIA"})
 
 # 3) Preprocesamiento de datos:
-    # Convertimos las variables a su tipo correspondiente:
+    # Se convierten las variables a su tipo correspondiente
 df['ID'] = df['ID'].astype(int)
 df['TEXTO'] = df['TEXTO'].astype(str)
 df['CATEGORIA'] = df['CATEGORIA'].astype(int)
-
-    # Preprocesado y limpieza de la columna TEXTO:
+    # Preprocesado y limpieza de la columna TEXTO
 preprocesamiento = Preprocesado(df,'TEXTO')
 df = preprocesamiento.limpieza()
 
 # 4) Clasificación y predicción:
-    # Se instancia el clasificador con el DataFrame y la columna de texto:
+    # Se crean las variables con las rutas de vector y modelo
 modelo_path_RL = "/ruta_del_modelo/modelo_TF_SDG_SW_L.pkl"  # Introducir ruta
 vectorizador_path_RL = "/ruta_del_vectorizador/vectorizador_TF_SDG_SW_L.pkl"  # Introducir ruta
 df_1 = df
 columna_texto_1 = 'TEXTO_STOPWORDS_LEMATIZACION'
+    # Se instancia el clasificador con el DataFrame y la columna de texto
 clasificador_RL = CLasificadorTexto(modelo_path=modelo_path_RL, vectorizador_path=vectorizador_path_RL, df=df_1, columna_texto=columna_texto_1)
 clasificaciones_RL = clasificador_RL.clasificar_columna_texto()
 probabilidades_RL= clasificador_RL.predecir_probabilidades_columna_texto()
@@ -53,13 +53,13 @@ mapeo_emociones = {
 df['CATEGORIA_MODELO'] = df['CATEGORIA_MODELO'].replace(mapeo_emociones)
 df['CATEGORIA'] = df['CATEGORIA'].replace(mapeo_emociones)
 
-    # Guardado
+    # Se guarda la tabla clasificada en la ruta que se desee especificar
 clasificacion_csv = '/ruta_de_destino/clasificacion_automatica.csv' # Introducir ruta
 if df is not None:
     guardar = GuardarCSV(df)
     guardar.guardar_dataframe(clasificacion_csv, columnas=columnas_deseadas) 
 
-    # Registra el tiempo de finalización
+    # Se registra el tiempo de finalización
 tiempo_fin = time.time()
     # Calcula el tiempo total de ejecución
 tiempo_total = tiempo_fin - tiempo_inicio
